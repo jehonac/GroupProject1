20e984294446a08c96ad4beaad75f657203b7ec0
Kristina Wong <kristina.wong.us@gmail.com>
2016-04-14 14:47:00 -0400

    updating changes

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
 
 public class MainApp {
+
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		String userInput = getUserInput();
+		System.out.println("What would you like to do? \ntype 1 if you want to encrypt a message. \n Type 2 if you want to decrypt a secret message. ");
+		
+		
+		
+		
+
+	}
+
+	public static String getUserInput() {
+
+		System.out.println("Enter a message to be encrypted: ");
+		String userInput = scan.next();
+		return userInput;
+	}
+
+	
+
+	public static void displayEncryptedText(String message) {
+
+	}
+
+	public static void displayDecryptedText(String message) {
+
+	}
 
 }
